6a4e86f2e8b639050563580ff09eab8163dc4094
Sergio Vazquez <alb.vazq@gmail.com>
2020-05-19 21:43:51 -0500

    Implemented channel disabling.

--- src/tca9548a.c
+++ src/tca9548a.c
@@ -4,11 +4,15 @@
 extern "C" {
 #endif
 void setMux(unsigned int muxAddress, unsigned int channel) {
-  // Set channel on sensor mux
-  i2c_start(*getBus());
-  i2c_send_byte(*getBus(), muxAddress << 1 | I2C_WRITE);
-  i2c_send_byte(*getBus(), 1 << channel);
-  i2c_stop(*getBus());
+    // Set channel on sensor mux
+    i2c_start(*getBus());
+    i2c_send_byte(*getBus(), muxAddress << 1 | I2C_WRITE);
+    if (channel == 0) {
+        i2c_send_byte(*getBus(), channel);
+    } else {
+        i2c_send_byte(*getBus(), 1 << (channel - 1));
+    }
+    i2c_stop(*getBus());
 }
 #ifdef __cplusplus
 }
